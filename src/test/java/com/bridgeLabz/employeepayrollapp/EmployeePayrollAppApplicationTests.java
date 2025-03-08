ee4ae879ce4a3d208ddf8370995d117cbc2ea59d
srijan0412 <srijan.soni01@gmail.com>
2025-03-08 09:59:00 +0530

    Initial Commit

---
+++ src/test/java/com/bridgeLabz/employeepayrollapp/EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.employeepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
